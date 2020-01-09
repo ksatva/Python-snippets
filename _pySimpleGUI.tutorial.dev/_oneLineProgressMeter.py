476d25462b7f0713957b32385809cc87909de654
k.ai <ksrt@outlook.com>
2020-01-09 23:55:04 +0530

    git ignore now working with command 'git rm -r --cached ./'

--- _pySimpleGUI.tutorial.dev/_oneLineProgressMeter.py
+++
@@ -1,4 +0,0 @@
-import PySimpleGUI as sg      
-
-for i in range(1000):      
-    sg.OneLineProgressMeter('One Line Meter Example', i+1, 1000, 'key')  
--- _ssh.dev/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-from dbMYDEVICESignature import DEVICEid, COMMANDSn
-from _ssh_dev import _ssh
-
-
-
-
-if __name__ == '__main__':
-	obj = _ssh('ubuntu_dev')
